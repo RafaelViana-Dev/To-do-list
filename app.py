f695ffd118b879b78510346d6b87957bf39d52a0
Rafael <programador1350@gmail.com>
2025-07-22 19:02:41 -0300

    Criado a primeira parte da API o GET

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
-from flask import Flask
-
+import sqlite3
+import json
+from flask import Flask, request, jsonify
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return "Olá, esta é minha primeira aplicação Flask!"
+
+database = 'sqlite-python/dados.db'
+
+@app.route('/todo/getall',methods=['GET'])
+def getTasks():
+    with sqlite3.connect(database) as conn:
+        cursor = conn.cursor()
+        
+        cursor.execute('SELECT * FROM task')
+        dados = cursor.fetchall()
+    return jsonify(dados)
+
+@app.route('/todo/create',methods=['POST'])
+def createTask():
+    return 'Create new task'
+
+@app.route('/todo/update',methods=['UPDATE'])
+def updateTask():
+    return 'Update task'
+
+@app.route('/todo/delete',methods=['DELETE'])
+def deleteTask():
+    return 'Delete task'
+
 if __name__ == '__main__':
     app.run(debug=True)
